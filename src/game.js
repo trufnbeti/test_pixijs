b8821395fcf5e1e6a379d586716b6c6c07a9af39
trufnbeti <trungboy80@gmail.com>
2023-06-02 11:23:53 +0700

    update

--- src/game.js
+++ src/game.js
@@ -1,13 +1,20 @@
-import { Application } from "pixi.js";
+import { Application, Sprite } from "pixi.js";
 
 export class Game {
     static init() {
         this.app = new Application({
             width: 720,
             height: 1280,
-            backgroundColor: 0x1099bb,
+            backgroundColor: "pink",
         });
         document.body.appendChild(this.app.view);
+        var clampy = Sprite.from("assets/images/clampy.png");
+        clampy.anchor.set(0.5);
+        clampy.x = this.app.screen.width / 2;
+        clampy.y = this.app.screen.height / 3;
+        clampy.scale.x = 0.5;
+        clampy.scale.y = 0.5;
+        this.app.stage.addChild(clampy);
     }
 }
 
